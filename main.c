a23a0710c7036e042cbc67c0cd2095fe5c84130f
kartofelr <johnmepno@gmail.com>
2026-04-13 14:16:57 +0200

    changed main to print salut

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include "operations.h"
-int main (){
-    int a = 1;
-    int b = 2;
-    printf("a = %d, b = %d\n",a,b);
-    printf("a + b = %d\n",plus(a,b));
-    printf("a * b = %d\n",mult(a,b));
-    printf("a^2 + b^2 = %d\n",quadra_plus(a,b));
+#include <stdio.h>
+int main() {
+  int a = 1;
+  int b = 2;
+  printf("salut");
+  printf("a = %d, b = %d\n", a, b);
+  printf("a + b = %d\n", plus(a, b));
+  printf("a * b = %d\n", mult(a, b));
+  printf("a^2 + b^2 = %d\n", quadra_plus(a, b));
 }
